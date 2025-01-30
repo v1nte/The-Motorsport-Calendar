78a35cbaa06ed773dcbfaa218557026114695826
v1nte <ivinte99@gmail.com>
2025-01-30 16:12:36 -0300

    gitlab pages setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,10 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://v1nte.gitlab.io',
+  base: '/the-motorsport-calendar',
+  outDir: 'public',
+  publicDir: 'static',
+
   integrations: [tailwind()]
 });
