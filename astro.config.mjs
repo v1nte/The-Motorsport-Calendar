4662e346dbf68b2afe5c5d7ab9cb3aa6f43f41f1
v1nte <ivinte99@gmail.com>
2025-02-14 10:56:45 -0300

    trying to fix n.42069

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://v1nte.github.io',
-  base: '/The-Motorsport-Calendar',
+  // base: '/The-Motorsport-Calendar',
   // outDir: 'public',
   // publicDir: 'static',
 
